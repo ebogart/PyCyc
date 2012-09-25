ca6fdccb0f3bf10a307faf3b7ee6c8489b3064fe
Eli Bogart <elb87@cornell.edu>
2012-09-25 10:54:14 -0400

    update version number; add license, url

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from distutils.core import setup
 
 setup(
     name='PyCyc',
-    version='0.0.9',
+    version='0.0.10',
     author='Eli Bogart',
     author_email='elb87@cornell.edu',
     packages=['pycyc','pycyc.test'],
+    url='https://github.com/ebogart/PyCyc',
+    license='LICENSE.txt',
     description='Python interface to Pathway Tools',
     long_description=open('README.txt').read()
     )
